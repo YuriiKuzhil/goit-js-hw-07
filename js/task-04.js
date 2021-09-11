d855b1d924cddbb7ecd99c6b9c465ab77985b5ec
Yurii <yuriikuzhil@gmail.com>
2021-09-11 20:11:52 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,11 +1,3 @@
-//   <div id="counter">
-//       <button type="button" data-action="decrement">-1</button>
-//       <span id="value">0</span>
-//       <button type="button" data-action="increment">+1</button>
-//     </div>
-
-
-
 const btnDecrementEl = document.querySelector('[data-action="decrement"]')
 const btnIncrementEl = document.querySelector('[data-action="increment"]')
 const counterEl = document.querySelector('#value')
