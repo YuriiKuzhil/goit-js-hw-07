8b2a1520509bacbc3a595d3d4295a0c8a4c6c3ee
Yurii <yuriikuzhil@gmail.com>
2021-09-11 14:53:31 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const listEl = document.querySelector('#ingredients')
+
+const listItemsEl = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li')
+  itemEl.textContent = ingredient
+  return itemEl
+})
+
+listEl.append(...listItemsEl)
