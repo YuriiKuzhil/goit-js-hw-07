0d122708fad40c779a53c599c8393b303d0a3305
Yurii <yuriikuzhil@gmail.com>
2021-09-18 11:11:10 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -8,3 +8,11 @@ allItemsEl.forEach(item => {
 })
 
 
+const list = document.querySelector('#categories');
+const items = list.querySelectorAll('.item');
+console.log(`В списке ${items.length} категории:`);
+items.forEach(node => {
+const h2 = node.querySelector('h2');
+const nodeItems = node.querySelectorAll('li');
+console.log(` - категория: ${h2.textContent} (количество элементов: ${nodeItems.length})`);
+});
