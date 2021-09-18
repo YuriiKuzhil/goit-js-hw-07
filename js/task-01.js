7fa633b4e18890be2eee58f888cc821924d913ba
Yurii <yuriikuzhil@gmail.com>
2021-09-18 11:27:50 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,19 @@
-const allItemsEl = document.querySelectorAll('.item')
-console.log(`В списке ${allItemsEl.length} категории.`)
+// const allItemsEl = document.querySelectorAll('.item')
+// console.log(`В списке ${allItemsEl.length} категории.`)
 
-allItemsEl.forEach(item => {
-   
-    console.log(`Категория: ${item.firstElementChild.textContent}`)
-    console.log(`Количество элементов: ${item.lastElementChild.children.length}`)
-})
+// allItemsEl.forEach(item => {
 
+//     console.log(`Категория: ${item.firstElementChild.textContent}`)
+//     console.log(`Количество элементов: ${item.lastElementChild.children.length}`)
+// })
 
 const list = document.querySelector('#categories');
 const items = list.querySelectorAll('.item');
 console.log(`В списке ${items.length} категории:`);
 items.forEach(node => {
-const h2 = node.querySelector('h2');
-const nodeItems = node.querySelectorAll('li');
-console.log(` - категория: ${h2.textContent} (количество элементов: ${nodeItems.length})`);
+  const h2 = node.querySelector('h2');
+  const nodeItems = node.querySelectorAll('li');
+  console.log(
+    ` - категория: ${h2.textContent} (количество элементов: ${nodeItems.length})`
+  );
 });
