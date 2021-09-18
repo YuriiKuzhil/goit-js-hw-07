d7f4942e4446e3b1ca04604428bb1bb1f765006e
Yurii <yuriikuzhil@gmail.com>
2021-09-18 11:27:47 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,26 @@
 const inputEl = document.querySelector('input');
 
+// const checkInputValue = event => {
+//   let inputValue = event.currentTarget.value.length;
+//   const correctSymbolsQuantities = Number.parseInt(inputEl.dataset.length);
+
+//   if (inputValue === correctSymbolsQuantities) {
+//     inputEl.classList.replace('invalid', 'valid');
+//   } else {
+//     inputEl.classList.remove('valid');
+//     inputEl.classList.add('invalid');
+//   }
+// };
+
 const checkInputValue = event => {
   let inputValue = event.currentTarget.value.length;
   const correctSymbolsQuantities = Number.parseInt(inputEl.dataset.length);
 
-  if (inputValue === correctSymbolsQuantities) {
-    inputEl.classList.replace('invalid', 'valid');
-  } else {
-    inputEl.classList.remove('valid');
-    inputEl.classList.add('invalid');
-  }
+  inputEl.classList.add('invalid');
+
+  inputValue === correctSymbolsQuantities
+    ? inputEl.classList.replace('invalid', 'valid')
+    : inputEl.classList.replace('valid', 'invalid');
 };
 
 inputEl.addEventListener('input', checkInputValue);
